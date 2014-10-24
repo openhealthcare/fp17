8dcfbb955b799a0fa6c2ecf3077c48c8b2532637
Ross Jones <ross@servercode.co.uk>
2014-10-24 21:40:11 +0100

    Yet more HTML. And a table, why did I use a table? That's going to come back and bite me on the arse.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,8 +5,16 @@ defmodule FP17.PageController do
     render conn, "index"
   end
 
-  def new(conn, _params) do
-    render conn, "new"
+  def new_step1(conn, _params) do
+    render conn, "new_one"
+  end
+
+  def new_step2(conn, _params) do
+    render conn, "new_two"
+  end
+
+  def new_step3(conn, _params) do
+    render conn, "new_three"
   end
 
   def not_found(conn, _params) do
--- web/router.ex
+++ web/router.ex
@@ -3,7 +3,13 @@ defmodule FP17.Router do
 
   get "/", FP17.PageController, :index, as: :pages
 
-  get "/new", FP17.PageController, :new, as: :pages
-  post "/new", FP17.PageController, :new, as: :pages
+  get "/new/one", FP17.PageController, :new_step1, as: :pages
+  post "/new/one", FP17.PageController, :new_step1, as: :pages
+
+  get "/new/two", FP17.PageController, :new_step2, as: :pages
+  post "/new/two", FP17.PageController, :new_step2, as: :pages
+
+  get "/new/three", FP17.PageController, :new_step3, as: :pages
+  post "/new/three", FP17.PageController, :new_step3, as: :pages
 
 end
